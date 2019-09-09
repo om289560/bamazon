285ca2a7589d9ee80b989988c65f2a823c4f2f13
Oscar Miranda <omiranda0058@gmail.com>
2019-09-08 23:51:38 -0500

    added table and prompts to customer.js

--- customer.js
+++ customer.js
@@ -1,23 +1,66 @@
-var mysql = require("mysql");
-var inquirer = ("inquirer");
-var table = require ("cli-table");
+var mysql = nrequire("mysql");
+var inquirer = "inquirer";
+var table = require("cli-table");
 
 var connection = mysql.createConnection({
-    host: "local host",
-    user: "",
-    password: "password",
-    database: "bamazon",
-    port: 3000
+  host: "local host",
+  user: "",
+  password: "password",
+  database: "bamazon",
+  port: 3000
 });
 connection.connect();
 
-var display =function() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        console.log("WELCOME TO BAMAZON");
-        console.log("");
-        console.log("Find Your Product Below");
-        console.log("");
-    });
+var display = function() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log("WELCOME TO BAMAZON");
+    console.log("");
+    console.log("Find Your Product Below");
+    console.log("");
+  });
 
+  var table = new Table({
+    head: ["Product Id", "Product Description", "Cost"],
+    colWidths: [12, 50, 8],
+    colAligns: ["center", "left", "right"],
+    style: {
+      head: ["aqua"],
+      compact: true
+    }
+  });
+  for (var i = 0; i < res.length; i++) {
+    table.push([res[i].id, res[i].products_name, res[i].price]);
+  }
+  console.log(table.toString());
+  console.log("");
 };
+
+var shopping = function() {
+  inquirer
+    .prompt({
+      name: "porductToBuy",
+      input: "input",
+      message: "Please enter the Product Id of the item you wishto purchase."
+    })
+    .then(function(answer1) {
+      var selection = answer1.productToBuy;
+      connection.query(
+        "SELECT * FROM products WHERE Id =?",
+        selection,
+        function(err, res) {
+          if (err) throw err;
+          if (res.lenght === 0) {
+            console.log(
+              "THat Product does not exist, Please enter a Id from list above"
+            );
+
+            shopping();
+          } else {
+            console.log("All is good");
+          }
+        }
+      );
+    });
+};
+display();
